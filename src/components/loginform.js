c40f1cf240bfac94f4ea16ff92991ad353e384b1
= <>
2020-12-28 13:35:06 -0500

    loginform done

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {RemoveScrollBar} from 'react-remove-scroll-bar';
 import { Button,
+        Alert,
         Form,
         FormGroup,
         Label,
@@ -9,28 +10,117 @@ import { Button,
         Container,
         Row,
         Col } from 'reactstrap';
+import api from '../api/axios';
 
-const form = () => {
-          return(
-            <React.Fragment>
-            <RemoveScrollBar />
-              <Form>
-                <FormGroup row>
-                  <Label for="exampleEmail" className = 'stylingText' sm={12}>Email</Label>
-                  <Col sm={{ size: 6, order: 0, offset: 3 }}>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="Enter Email..." />
-                  </Col>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="examplePassword" className = 'stylingText'sm={12}>Password</Label>
-                  <Col sm={{ size: 6, order: 0, offset: 3 }}>
-                  <Input type="password" name="password" id="examplePassword" placeholder="Enter password..." />
-                  </Col>
-                </FormGroup>
-              </Form>
-              <Button color = "primary" size = "lg">Login</Button>
-            </React.Fragment>
-          )
-        }
+const FormsTwo = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ visible, setVisible ] = useState(false);
+  const [ disable, setDisable ] = useState(false);
+  const [ error, setError ] = useState(false);
+  const [ exist, setExist ] = useState(true);
+  const [ wrongpassword, setWrongpassword ] = useState(false);
+  const [ dodisable, setdoDisable ] = useState(false);
 
-export default form;
+  const changeEmail = (event) =>{
+    setEmail(event.target.value);
+  }
+
+  const changePassword = (event) =>{
+    setPassword(event.target.value);
+  }
+
+  const submitForm = (event) =>{
+    event.preventDefault();
+    setError(false);
+    setExist(true);
+    setWrongpassword(false);
+    setdoDisable(true);
+    if (email === '' || password === '') {
+      setError(true);
+      setVisible(true);
+      setTimeout(()=>{
+        setVisible(false);
+        setdoDisable(false)
+      }, 2000);
+      return;
+    }
+    api.post('/api/user/login', {
+      email:email,
+      password:password
+    })
+    .then(res=>{
+      console.log(res.data.message);
+      if (res.data.message === 'User does not exist') {
+        setExist(false);
+        setVisible(true);
+        setTimeout(()=>{
+          setVisible(false);
+          setdoDisable(false)
+        }, 2000);
+        return;
+      }else if (res.data.message === 'Enter correct credential') {
+        setWrongpassword(true);
+        setVisible(true);
+        setTimeout(()=>{
+          setVisible(false);
+          setdoDisable(false)
+        }, 2000)
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+  return(
+    <React.Fragment>
+      {
+        error ?
+        <Alert color = "danger" isOpen = { visible }>
+          Please enter all the fields correctly.
+        </Alert>
+        :
+        null
+      }
+      {
+        !error && !exist ?
+        <Alert color = "danger" isOpen = { visible }>
+         User does not exist. Please register.
+        </Alert>
+        :
+        null
+      }
+      {
+        !error && exist && wrongpassword ?
+        <Alert color = "danger" isOpen = { visible }>
+          Please enter correct credential.
+        </Alert>
+        :
+        null
+      }
+      <RemoveScrollBar />
+      <Form>
+        <FormGroup row>
+          <Label for="exampleEmail" className = 'stylingText' sm={12}>Email</Label>
+          <Col sm={{ size: 6, order: 0, offset: 3 }}>
+          <Input onChange = { changeEmail } value = { email } type="email" name="email" id="exampleEmail" placeholder="Enter Email..." />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword" className = 'stylingText'sm={12}>Password</Label>
+          <Col sm={{ size: 6, order: 0, offset: 3 }}>
+            <Input onChange = { changePassword } value = { password } type="password" name="password" id="examplePassword" placeholder="Enter password..." />
+          </Col>
+        </FormGroup>
+        </Form>
+      {
+        !dodisable ?
+        <Button disabled = { false } onClick = { submitForm } color = "primary" size = "lg">Login</Button>
+        :
+        <Button disabled = { true } onClick = { submitForm } color = "primary" size = "lg">Logging...</Button>
+      }
+    </React.Fragment>
+  )
+}
+
+export default FormsTwo;
--- src/components/registerform.js
+++ src/components/registerform.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {RemoveScrollBar} from 'react-remove-scroll-bar';
 import { Button,
-        Fade,
         Alert,
         Form,
         FormGroup,
@@ -11,13 +10,16 @@ import { Button,
 import { stylingText } from '../styles/registerform.css';
 import api from '../api/axios';
 
-const Forms = () => {
+
+const Forms = (props) => {
   const [ email, setEmail ] = useState('');
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ correctCredential, setCorrectCredential ] = useState(true);
   const [ exist, setExist ] = useState(false);
   const [visible, setVisible] = useState(true);
+  const [success, setSuccess ] = useState(false);
+  const [disable, setDisable ] = useState(false);
 
   const changeEmail = (event) =>{
     setEmail(event.target.value);
@@ -34,15 +36,18 @@ const Forms = () => {
 
   const submitForm = (event) =>{
     event.preventDefault();
+    setDisable(true);
     if (email && username && password) {
       setCorrectCredential(true);
     }
-    if (email == '' || username == '' || password == '') {
+    if (email === '' || username === '' || password === '') {
       setVisible(true);
       setCorrectCredential(false);
       setTimeout(()=>{
         setVisible(false)
+        setDisable(false)
       }, 2000)
+
       return;
     }
     api.post("/api/user/register/", {
@@ -51,32 +56,45 @@ const Forms = () => {
       password:password
     })
     .then(res=>{
-      if (res.data.message == 'Registration failed') {
+      console.log(disable);
+      if (res.data.message === 'Registration failed') {
         setCorrectCredential(false);
         setVisible(true)
         setTimeout(()=>{
           setVisible(false)
+          setDisable(false)
         }, 2000)
-      }else if (res.data.message == 'User Already exists') {
+
+        return;
+      }else if (res.data.message === 'User Already exists') {
         setVisible(true);
         setExist(true);
         setTimeout(()=>{
           setVisible(false)
+          setDisable(false)
         }, 2000)
+
         return;
+      }else{
+        setExist(false)
+        setVisible(true);
+        setSuccess(true);
+        setTimeout(()=>{
+          setVisible(false)
+          setDisable(false);
+        }, 2000)
       }
     })
     .catch(err=>{
       console.log(err);
     })
-
   }
   return(
     <React.Fragment>
     {
       !correctCredential ?
         <Alert color = "danger" isOpen= { visible }>
-          Please fill all the fields correctly.
+          Please fill all the fields correctly. Failed to register.
         </Alert>
       :
       null
@@ -86,7 +104,15 @@ const Forms = () => {
         <Alert color = "danger" isOpen = { visible }>
           User already exists. Can't register.
         </Alert>
-        :
+      :
+      null
+    }
+    {
+      success && !exist && correctCredential ?
+      <Alert color = "success" isOpen = { visible }>
+        Successfully registered. Go to Login!!
+      </Alert>
+      :
       null
     }
     <RemoveScrollBar />
@@ -110,7 +136,12 @@ const Forms = () => {
           </Col>
         </FormGroup>
       </Form>
-      <Button onClick = { submitForm } color = "primary" size = "lg">Register</Button>
+      {
+        !disable ?
+        <Button disabled = { false } onClick = { submitForm } color = "primary" size = "lg">Register</Button>
+        :
+        <Button disabled ={ true } onClick = { submitForm } color = "primary" size = "lg">Registering...</Button>
+      }
     </React.Fragment>
   )
 }
